a5c6c7034a6f3236b45a96ac12c041cf40709484
saikumarkusangi <saikumarkusangi007@gmail.com>
2023-06-10 23:41:49 +0530

    tutuorials

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const students ={ 
+  data:[
+  {'name':'sai','age':20,'branch':'eee'},
+  {'name':'bubby','age':20,'branch':'eee'},
+  {'name':'sandeep','age':21,'branch':'csm'}
+]
+}
+const staff ={
+  data: [
+  {'name':'rayudu','age':40,'branch':'eee','post':'Hod'},
+  {'name':'rama krishna','age':41,'branch':'eee','post':'proffesor'}
+]
+}
+app.get('/bvrit/:year/:type',(req,res)=>{
+if(req.params.type === 'students'){
+const query = req.query;
+var filtered = students.data.filter(function(user){
+  console.log(query.name)
+  try {
+    if(user.name === query.name && user.branch===query.branch){
+      return user;
+     }
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+res.writeHead(200,{"Content-Type":"application/json"})
+res.end(JSON.stringify(filtered))
+}else if(req.params.type === 'staff'){
+  res.writeHead(200,{"Content-Type":"application/json"})
+  res.end(JSON.stringify(staff))
+}
+else{
+  res.writeHead(404,{"Content-Type":"application/json"})
+  res.end(JSON.stringify({'status code':404,'reason':'page not found'}))
+}
+})
+app.listen(3000,()=>{
+  console.log('server running on port 3000')
+}); 
