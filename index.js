ded6ad7b526b2e29cca777f77e7146d7bbba6b7d
saikumarkusangi <saikumarkusangi007@gmail.com>
2023-06-11 13:14:48 +0530

    env added

--- index.js
+++ index.js
@@ -1,43 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
 const app = express();
-const students ={ 
-  data:[
-  {'name':'sai','age':20,'branch':'eee'},
-  {'name':'bubby','age':20,'branch':'eee'},
-  {'name':'sandeep','age':21,'branch':'csm'}
-]
-}
-const staff ={
-  data: [
-  {'name':'rayudu','age':40,'branch':'eee','post':'Hod'},
-  {'name':'rama krishna','age':41,'branch':'eee','post':'proffesor'}
-]
-}
-app.get('/bvrit/:year/:type',(req,res)=>{
-if(req.params.type === 'students'){
-const query = req.query;
-var filtered = students.data.filter(function(user){
-  console.log(query.name)
-  try {
-    if(user.name === query.name && user.branch===query.branch){
-      return user;
-     }
-  } catch (error) {
-    console.log(error)
-  }
+const port = process.env.PORT || 3000;
+
+app.get('/',(req,res)=>{
+  res.end('hello from node')
 })
 
-res.writeHead(200,{"Content-Type":"application/json"})
-res.end(JSON.stringify(filtered))
-}else if(req.params.type === 'staff'){
-  res.writeHead(200,{"Content-Type":"application/json"})
-  res.end(JSON.stringify(staff))
-}
-else{
-  res.writeHead(404,{"Content-Type":"application/json"})
-  res.end(JSON.stringify({'status code':404,'reason':'page not found'}))
-}
+
+
+app.listen(port,()=>{
+  console.log(`server is running on port ${port}`)
 })
-app.listen(3000,()=>{
-  console.log('server running on port 3000')
-}); 
